07ff65e8605cf4e226c40b9a4d4f5750fb7264e7
Jesuloba <jesulobajohn@gmail.com>
2020-10-30 02:43:03 +0100

    Pure Component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-/* const persons = (props) => {
-        console.log("[persons.js] rendering...")
 
-        return props.persons.map((person, index) => {
-                return <Person 
-                        click={() => props.delete(index)}
-                        name={person.name}
-                        age={person.age}
-                        key={person.id} 
-                        changed={(event) => props.changed(event, person.id)} 
-                />
-        });
-}
 
-export default persons;
-*/
-
-class Persons extends Component {
+class Persons extends PureComponent {
     // static getDerivedStateFromProps(props, state) {
     //     console.log("[Persons.js] getDerivedStateFromProps");
     //     return state;
     // } 
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("[Persons.js] shouldComponentUpdate");
-        if (nextProps.persons !== this.props.persons) {
-            return true;
-        }
-        return false;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("[Persons.js] shouldComponentUpdate");
+    //     if (nextProps.persons !== this.props.persons) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
         console.log("[Persons.js] getSnapshotBeforeUpdate");
